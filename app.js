4dc33ed4acfb4f035329a0891520001f46d608b1
Phillip Huang <phillip96huang@gmail.com>
2020-02-17 23:35:21 -0500

    Implement basic backend user authentication

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const db = require('./config/keys').mongoURI;
+const usePassport = require('./config/passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Welcome to TTP stocks"));
+app.use(passport.initialize());
+usePassport(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
