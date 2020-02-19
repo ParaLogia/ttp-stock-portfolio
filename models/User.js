b7cdcbde7e9caec8ed9bf4812e47d916f38cc521
Phillip Huang <phillip96huang@gmail.com>
2020-02-19 10:58:53 -0500

    Add balance attribute to User model

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  balance: {
+    type: Schema.Types.Decimal128,
+    default: "5000.00"
+  },
   date: {
     type: Date,
     default: Date.now
